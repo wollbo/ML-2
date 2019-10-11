6c440ddf9872b27fd77146f60211d9c9fa1070c1
wollbo <Hilding@n159-p200.eduroam.kth.se>
2019-10-11 11:11:19 +0200

    add algo

--- sumprod/maxsum.m
+++ sumprod/maxsum.m
@@ -3,25 +3,46 @@ addpath datasets/image
 folder = 'datasets/image';
 imageMatrix = imread([folder '/lena_std'],'tif');
 imageMatrix = rgb2gray(imageMatrix);
-trueMatrix = imageMatrix;
+trueMatrix = double(imageMatrix);
 N = 256; %8bit
+M = length(imageMatrix);
 
-%%
+%% Gaussian noise
 
-noiseMatrix = randn(512)*0.1;
+noiseMatrix = randn(M)*40;
 flippedMatrix = round(double(imageMatrix) + noiseMatrix);
+flippedMatrix(flippedMatrix<1) = 1;
+flippedMatrix(flippedMatrix>256) = 256;
+
+%% GF(2^k) noise
+
+p = 0.1;
+flipM=binornd(1,p*ones([M M]));
+flipped = imageMatrix(flipM==1);
+flipped = randi(N,length(flipped),1,'uint8');
+imageMatrix(flipM==1) = flipped;
+flippedMatrix = imageMatrix;
+
+%% Create image Chain
+
 imageChain = makeChain(flippedMatrix);
 decodedChain = zeros(size(imageChain));
 L = length(imageChain);
 
-%% p(y|x) assuming x uniformly distributed
+%% p(y|x) assuming x uniformly distributed, gaussian noise case
 
 support = 1:256;
 K = length(support);
 priorProbs = zeros(K);
 for k = 1:K
-    priorProbs(k,:) = normpdf(support,support(k),2); % gaussian noise case
+    priorProbs(k,:) = normpdf(support,support(k),2);
 end
+
+%% p(y|x) assuming x uniformly distributed, GF(2^K) noise case
+
+support = 1:256;
+K = length(support);
+priorProbs = 1/N * ones(K) + eye(K)*(1-p-1/N);
 
 %% p(x(k)|x(k-1)) from histogram
 
@@ -44,13 +65,21 @@ muPhi = log(priorProbs(imageChain(1),:)); % log necessary in larger images
 muXF(1,:) = muPhi;
 indX = zeros(L,1);
 indX(1) = imageChain(1);
-for l = 2:L % forward loop
+for l = 2:L % forward loop, only smoothes the picture without removing noise
     [muFX(l),indX(l)] = max(log(histProbs(imageChain(l),:)) + muXF(l-1,:));
-    muXF(l,:) = muFX(l)*log(priorProbs(imageChain(1),:));
+    muXF(l,:) = muFX(l);%+log(priorProbs(imageChain(l),:));
 end
 disp('done')
+
+%% could use a backward loop to update indX as x(n-1) | x(n) from x(N) to x(1)
+
+
+
 %%
 
 imageMatrix2 = uint8(makeImage(indX));
+noisyMatrix = uint8(flippedMatrix);
 imshow(imageMatrix2)
-mean(mean((imageMatrix-trueMatrix).^2))
+imshow(noisyMatrix)
+mse1 = mean(mean((double(imageMatrix2)-trueMatrix).^2))
+mse2 = mean(mean((double(noisyMatrix)-trueMatrix).^2))
